0156c984d52065b9c6f4aae7fb6dc4498af16e1f
dci <rudeshdhote@gmail.com>
2021-04-14 15:08:54 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,119 @@
+// 1
+
+const num1 = 50;
+
+const num2 = 99;
+
+if (result1 = ((num1 >= 50 && num1 <= 99) || (num2 >= 50 && num2 <= 99))) {
+    
+    console.log(result1);
+} 
+
+
+// 2
+const num11 = 50 ;
+
+const num22 = 55;
+
+const num33 = 88;
+
+
+if (result2 = ((num11 >= 50 && num11 <= 99)) || ((num22 >= 50) && (num22 <= 99)) || ((num33 >= 50) && (num33 <= 99))) {
+    console.log(result2)
+} 
+
+
+// 3
+
+const a = 23;
+
+const b = 44;
+
+const c = 36;
+
+if (max = Math.max(a, b, c)) {
+    console.log(max)
+}
+
+
+
+// 4
+
+const city = "dubai";
+
+const add = "Py"+ city
+
+if (add.slice(2, 7)) {
+    console.log(city)
+
+}
+
+//5 
+
+const num111 = 20;
+
+const num222 = 50;
+
+if (((a+b) > 50) && ((a+b) < 80)) {
+
+    console.log(65)
+    
+} else {
+    console.log(80)
+}
+
+
+//6
+
+const num1111 = 2;
+
+const num2222 = 6;
+
+if (((num1111+num2222) === 8) || ((num1111-num2222) === 8)) {
+    console.log(true)
+}
+
+//7
+
+const num12 = 10;
+
+const num13 = 5;
+
+if ((num12 === 15) || (num13 === 15) || (num12 + num13) === 15 ) {
+    
+    console.log(true)
+}
+
+//8
+
+const num123 = 56;
+
+const num234 = 6;
+
+if (((num123 % 7 === 0 || num123 % 11 === 0)) || ((num234 % 7 === 0 || num234 % 11 === 0))) {
+    console.log(true)
+}
+
+//9
+
+num1123 = 55;
+
+num1234 = 66;
+
+const adding = num1123 + num1234 ;
+
+if (num1123 = num1234) {
+    
+   console.log( adding * 3)
+}
+
+//10
+
+const num2123 = 44;
+
+const num2124 = (num2123 - 19 )
+
+if (num2123 > 19) {
+    
+    console.log(num2124 * 2)
+}
